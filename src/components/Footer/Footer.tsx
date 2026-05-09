b9daf496a938420be573f1782b5474b9fe4eb5f6
sdrscom <it_support@sdrs.com.sa>
2026-05-09 13:45:02 +0500

    Fix Footer logo path v2

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import { Phone, Mail, MapPin } from "lucide-react";
 import { useLanguage } from "../../context/LanguageContext";
 import { translations } from "../../locales/translations";
-import logo from "/src/assets/Footer-logo.png";
+import logo from "../../assets/Footer-logo.png";
 
 export default function Footer() {
   const { language } = useLanguage();
